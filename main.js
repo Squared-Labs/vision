f26a4bb7123932a40d3a150f3b5bc4967bce3c5f
SaturdayNightDead <ihubnetceogeenomc@gmail.com>
2020-02-21 20:46:21 -0800

    Base v1 first rel

---
+++ main.js
@@ -0,0 +1,38 @@
+// Load Electron modules
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 1024,
+    frame: false,
+    show: true,
+    webPreferences: {
+      webviewTag: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // Load browser
+  mainWindow.loadFile('browser.html')
+
+  // devtools
+  // mainWindow.webContents.openDevTools()
+}
+
+// Window create
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+// When all windows are closed, thje app quits unless if platform = Darwin (macOS)
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  // For macOS: If app is still open, when program launched, just create a new window.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+// NOTE TO SELF: include more code if needed
